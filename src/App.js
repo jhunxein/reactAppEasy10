b22f067e46378b2d21002964510563332233883e
jhunxein <jhunsensamonte@gmail.com>
2021-04-04 21:52:32 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/app.css';
+
+import user from './assets/images/image-victor.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<main className="container--wrapper">
+				<section className="card">
+					<div className="card--top__box"></div>
+					<div className="card--top__wrapper">
+						<img src={user} alt="..." className="card--top__img" />
+						<h1 className="card--top__title">
+							<span>Victor Crest</span> 26
+						</h1>
+						<p className="card--top__desc">London</p>
+					</div>
+					<div className="card--status">
+						<div className="card--status--wrapper">
+							<h2 className="card--status__title">
+								<span>80K</span> Followers
+							</h2>
+						</div>
+						<div className="card--status--wrapper">
+							<h2 className="card--status__title">
+								<span>803K</span> Likes
+							</h2>
+						</div>
+						<div className="card--status--wrapper">
+							<h2 className="card--status__title">
+								<span>1.4K</span> Photos
+							</h2>
+						</div>
+					</div>
+				</section>
+			</main>
+		</div>
+	);
 }
 
 export default App;
